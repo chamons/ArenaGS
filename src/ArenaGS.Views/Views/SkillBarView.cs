fa0543bcf262633096f9a555835bb4abca1f0f61
Chris Hamons <chris.hamons@gmail.com>
2017-05-29 21:27:19 -0500

    Fixup skillbar to look ok enough to continue

--- src/ArenaGS.Views/Views/SkillBarView.cs
+++ src/ArenaGS.Views/Views/SkillBarView.cs
@@ -12,32 +12,38 @@ namespace ArenaGS.Views.Views
 		const int NumberOfSkills = 15;
 		const int Padding = 2;
 		const int CellSize = 32;
+		const bool ShowHotkey = true;
 
 		string [] CellLabels = new string [NumberOfSkills] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "[", "]", "\\" };
 		string [] CellImages = new string [NumberOfSkills] { "burning-dot.png", "burning-meteor.png", "wave-crest.png", "wind-hole.png", "snowing.png", "lightning-branches.png", "blunderbuss.png", "grenade.png", "firework-rocket.png", "materials-science.png", "missile-mech.png", "missile-swarm.png", "ray-gun.png", "sentry-gun.png", "armor-vest.png" };
 
+		SKPaint BlackPaint = new SKPaint () { Color = SKColors.Black };
+		SKPaint AntialiasPaint = new SKPaint () { IsAntialias = false };
+		SKPaint CellText = new SKPaint () { Color = SKColors.White, TextSize = 10, TextAlign = SKTextAlign.Center };
+		SKPaint CellBorder = new SKPaint () { Color = SKColors.White, StrokeWidth = 2, IsStroke = true };
 
 		public override SKSurface Draw (GameState state)
 		{
-			using (SKPaint cellText = new SKPaint () { Color = SKColors.White, TextSize = 10, TextAlign = SKTextAlign.Center })
+			for (int i = 0; i < NumberOfSkills; ++i)
 			{
-				using (SKPaint cellBorder = new SKPaint () { Color = SKColors.White, StrokeWidth = 2, IsStroke = true })
+				int left = Padding + ((Padding + CellSize) * i);
+				int top = Padding;
+				int right = left + CellSize + Padding;
+				int bottom = top + CellSize + Padding;
+
+				Canvas.DrawRect (new SKRect (left, top, right, bottom), CellBorder);
+
+				Canvas.DrawBitmap (Resources.Get (CellImages[i]), new SKRect (left + Padding, top + Padding, right - Padding, bottom - Padding), AntialiasPaint);
+
+				if (ShowHotkey)
 				{
-					for (int i = 0; i < NumberOfSkills; ++i)
-					{
-						int left = Padding + ((Padding + CellSize) * i);
-						int top = Padding;
-						int right = left + CellSize + Padding;
-						int bottom = top + CellSize + Padding;
-
-						Canvas.DrawRect (new SKRect (left, top, right, bottom), cellBorder);
-
-						Canvas.DrawBitmap (Resources.Get (CellImages[i]), new SKRect (left + Padding, top + Padding, right - Padding, bottom - Padding));
-
-						Canvas.DrawText (CellLabels[i], left + (CellSize / 2), CellSize - Padding + 1, cellText);
-					}
+					int textLeft = left + (CellSize / 2);
+					int textTop = CellSize + Padding + 3;
+					Canvas.DrawRect (new SKRect (textLeft - 3, textTop - 8, textLeft + 4, textTop + 8), BlackPaint);
+					Canvas.DrawText (CellLabels [i], textLeft, CellSize + Padding + 3, CellText);
 				}
 			}
+
 			return Surface;
 		}
 	}
