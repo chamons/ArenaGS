26520d8ae1bf7f21e6d0b697f27cdbc3f97a4d79
Chris Hamons <chris.hamons@gmail.com>
2017-09-04 16:37:26 -0500

    Refactor TargettingInfo

--- src/ArenaGS/Engine/CharacterLibrary.cs
+++ src/ArenaGS/Engine/CharacterLibrary.cs
@@ -26,11 +26,11 @@ namespace ArenaGS
 
 			Character player = Generator.CreatePlayer (Point.Empty, new Health (3, 3), new Defense (1));
 			player = player.WithSkills (new Skill[] {
-				Generator.CreateSkill ("Shot", Effect.Damage, new DamageSkillEffectInfo (2), new TargettingInfo (TargettingStyle.Point, 8), new SkillResources (maxCooldown : 3)),
-				Generator.CreateSkill ("Grenade", Effect.Damage, new DamageSkillEffectInfo (1), new TargettingInfo (TargettingStyle.Point, 4, 3), new SkillResources (maxAmmo : 2)),
-				Generator.CreateSkill ("Dragon's Breath", Effect.Damage, new DamageSkillEffectInfo (1), new TargettingInfo (TargettingStyle.Cone, 3), new SkillResources (maxCooldown : 5)),
-				Generator.CreateSkill ("Delayed Blast", Effect.DelayedDamage, new DelayedDamageSkillEffectInfo (3), new TargettingInfo (TargettingStyle.Point, 3, 1), new SkillResources (maxAmmo : 2)),
-				Generator.CreateSkill ("Line Strike", Effect.Damage, new DamageSkillEffectInfo (3), new TargettingInfo (TargettingStyle.Line, 3), new SkillResources (maxCooldown : 2)),
+				Generator.CreateSkill ("Shot", Effect.Damage, new DamageSkillEffectInfo (2), TargettingInfo.Point (8), new SkillResources (maxCooldown : 3)),
+				Generator.CreateSkill ("Grenade", Effect.Damage, new DamageSkillEffectInfo (1), TargettingInfo.Point (4, 3), new SkillResources (maxAmmo : 2)),
+				Generator.CreateSkill ("Dragon's Breath", Effect.Damage, new DamageSkillEffectInfo (1), TargettingInfo.Cone (3), new SkillResources (maxCooldown : 5)),
+				Generator.CreateSkill ("Delayed Blast", Effect.DelayedDamage, new DelayedDamageSkillEffectInfo (3), TargettingInfo.Point (3, 1), new SkillResources (maxAmmo : 2)),
+				Generator.CreateSkill ("Line Strike", Effect.Damage, new DamageSkillEffectInfo (3), TargettingInfo.Line (3), new SkillResources (maxCooldown : 2)),
 				Generator.CreateSkill ("Dash"),
 				Generator.CreateSkill ("Point Blank Shot"),
 				Generator.CreateSkill ("Charge"),
