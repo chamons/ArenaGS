2ed53801aebb668080f85d65d5de77ca383f02c5
Chris Hamons <chris.hamons@gmail.com>
2020-04-24 21:34:27 -0500

    Clean up some errors

--- src/after_image/image_loader.rs
+++ src/after_image/image_loader.rs
@@ -1,15 +1,16 @@
-use super::RenderContext;
+use super::{BoxResult, RenderContext};
 
 use sdl2::image::LoadTexture;
 use sdl2::render::Texture;
 
-fn get_exe_folder() -> Option<std::path::PathBuf> {
-    let exe_path = std::env::current_exe().unwrap();
-    Some(exe_path.parent()?.to_path_buf())
+fn get_exe_folder() -> BoxResult<std::path::PathBuf> {
+    let exe_path = std::env::current_exe()?;
+    let exe_folder = exe_path.parent().ok_or_else(|| ("No Parent Exe Folder"))?;
+    Ok(exe_folder.to_path_buf())
 }
 
-pub fn load_image<'r>(path: &str, render_context: &RenderContext) -> Result<Texture, String> {
-    let dest_path = get_exe_folder().unwrap().join(path);
+pub fn load_image<'r>(path: &str, render_context: &RenderContext) -> BoxResult<Texture> {
+    let dest_path = get_exe_folder()?.join(path);
 
     let texture_creator = render_context.canvas.texture_creator();
     Ok(texture_creator.load_texture(dest_path)?)
--- src/after_image/mod.rs
+++ src/after_image/mod.rs
@@ -5,3 +5,5 @@ mod render_context;
 pub use image_loader::load_image;
 pub use message_pump::{pump_messages, EventStatus};
 pub use render_context::RenderContext;
+
+pub type BoxResult<T> = Result<T, Box<dyn std::error::Error>>;
