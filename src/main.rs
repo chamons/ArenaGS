c81c8d09508f7ccdd373ce14e43c663273ec603a
Chris Hamons <chris.hamons@gmail.com>
2020-04-25 17:39:25 -0500

    Add panic hooks

--- src/main.rs
+++ src/main.rs
@@ -58,7 +58,37 @@ impl Scene for BattleScene {
     }
 }
 
+#[cfg(target_os = "windows")]
+fn open_url(url: &str) -> bool {
+    if let Ok(mut child) = std::process::Command::new("cmd.exe").arg("/C").arg("start").arg("").arg(&url).spawn() {
+        std::thread::sleep(std::time::Duration::new(1, 0));
+        if let Ok(status) = child.wait() {
+            return status.success();
+        }
+    }
+    false
+}
+
 pub fn main() -> Result<(), String> {
+    std::env::set_var("RUST_BACKTRACE", "1");
+    panic::set_hook(Box::new(|panic_info| {
+        let mut debug_spew = String::new();
+        if let Some(location) = panic_info.location() {
+            debug_spew.push_str(&format!("{} Line: {}\n", location.file(), location.line())[..]);
+        }
+        let payload = panic_info.payload();
+        if let Some(s) = payload.downcast_ref::<&str>() {
+            debug_spew.push_str(s);
+        } else if let Some(s) = payload.downcast_ref::<String>() {
+            debug_spew.push_str(s);
+        }
+
+        let _ = fs::write("debug.txt", debug_spew);
+
+        #[cfg(target_os = "windows")]
+        open_url("debug.txt");
+    }));
+
     let mut render_context = RenderContext::initialize()?;
 
     let scene = BattleScene::init(&mut render_context).unwrap();
