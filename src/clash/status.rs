0fccba1715e532bccfe6c70348e632d3a6dd0be7
Chris Hamons <chris.hamons@xamarin.com>
2020-08-18 12:48:21 -0400

    Implement most of status system, just needs ecs integration

--- src/clash/status.rs
+++ src/clash/status.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
 use specs::prelude::*;
 
@@ -10,25 +12,75 @@ pub enum StatusKind {
 }
 
 #[derive(Serialize, Deserialize, Clone)]
-pub struct StatusStore {}
+pub struct StatusStore {
+    store: HashMap<String, StatusKind>,
+}
 
 impl StatusStore {
     pub fn init() -> StatusStore {
-        StatusStore {}
+        StatusStore { store: HashMap::new() }
     }
 
-    pub fn add_status(&mut self, name: &str, length: u32) {}
+    pub fn add_status(&mut self, name: &str, length: i32) {
+        self.store
+            .entry(name.to_string())
+            .and_modify(|e| match e {
+                StatusKind::Status(timer) => timer.extend_to_duration(timer.duration()),
+                StatusKind::Trait => panic!("Status insert of {} but already in as a trait?", name),
+            })
+            .or_insert(StatusKind::Status(TickTimer::init_with_duration(length)));
+    }
 
-    pub fn add_trait(&mut self, name: &str) {}
+    pub fn add_trait(&mut self, name: &str) {
+        self.store
+            .entry(name.to_string())
+            .and_modify(|e| match e {
+                StatusKind::Status(_) => panic!("Status insert of {} but already as a status?", name),
+                StatusKind::Trait => {}
+            })
+            .or_insert(StatusKind::Trait);
 
-    pub fn remote_trait(&mut self, name: &str) {}
+        self.store.insert(name.to_string(), StatusKind::Trait);
+    }
+
+    pub fn remote_trait(&mut self, name: &str) {
+        match self.store.get(name) {
+            Some(StatusKind::Status(_)) => panic!("Status removal of trait {} but already as a status?", name),
+            None => panic!("Status remove of trait {} but not found?", name),
+            Some(StatusKind::Trait) => {}
+        };
+        self.store.remove(name);
+    }
 
     pub fn has(&self, name: &str) -> bool {
-        true
+        self.store.contains_key(name)
     }
 
-    pub fn duration(&self, name: &str) -> Option<u32> {
-        None
+    pub fn duration(&self, name: &str) -> Option<i32> {
+        match self.store.get(name) {
+            Some(StatusKind::Status(timer)) => Some(timer.duration()),
+            Some(StatusKind::Trait) => None,
+            None => None,
+        }
+    }
+
+    pub fn apply_ticks(&mut self, ticks: i32) {
+        let mut remove = vec![];
+
+        for (k, v) in self.store.iter_mut() {
+            match v {
+                StatusKind::Status(timer) => {
+                    if timer.apply_ticks(ticks) {
+                        remove.push(k.to_string());
+                    }
+                },
+                StatusKind::Trait => {}
+            }
+        }
+        
+        for r in remove {
+            self.store.remove(&r);
+        }
     }
 }
 
@@ -40,41 +92,87 @@ mod tests {
     #[test]
     fn add_status() {
         let mut store = StatusStore::init();
+        assert!(!store.has("TestStatus"));
         store.add_status("TestStatus", 200);
         assert!(store.has("TestStatus"));
+        assert_eq!(200, store.duration("TestStatus").unwrap());
     }
 
     #[test]
     fn add_duplicate_status() {
-        let store = StatusStore::init();
+        let mut store = StatusStore::init();
+        assert!(!store.has("TestStatus"));
+        store.add_status("TestStatus", 300);
+        store.add_status("TestStatus", 200);
+        assert!(store.has("TestStatus"));
+        assert_eq!(300, store.duration("TestStatus").unwrap());
     }
 
     #[test]
     fn add_trait() {
-        let store = StatusStore::init();
+        let mut store = StatusStore::init();
+        assert!(!store.has("TestTrait"));
+        store.add_trait("TestTrait");
+        assert!(store.has("TestTrait"));
     }
 
     #[test]
     fn add_duplicate_trait() {
-        let store = StatusStore::init();
+        let mut store = StatusStore::init();
+        store.add_trait("TestTrait");
+        store.add_trait("TestTrait");
+        assert!(store.has("TestTrait"));
+    }
+
+    #[should_panic]
+    #[test]
+    fn add_trait_existing_status() {
+        let mut store = StatusStore::init();
+        store.add_status("TestTrait", 100);
+        store.add_trait("TestTrait");
+    }
+
+    #[should_panic]
+    #[test]
+    fn add_status_existing_trait() {
+        let mut store = StatusStore::init();
+        store.add_trait("TestTrait");
+        store.add_status("TestTrait", 100);
     }
 
     #[test]
     fn remove_trait() {
-        let store = StatusStore::init();
+        let mut store = StatusStore::init();
+        store.add_trait("TestTrait");
+        assert!(store.has("TestTrait"));
+        store.remote_trait("TestTrait");
+        assert!(!store.has("TestTrait"));
+    }
+
+    #[should_panic]
+    #[test]
+    fn remove_trait_but_was_status() {
+        let mut store = StatusStore::init();
+        store.add_status("TestTrait", 100);
+        store.remote_trait("TestTrait");
     }
 
     #[test]
     #[should_panic]
     fn remove_non_existant_trait() {
-        let store = StatusStore::init();
+        let mut store = StatusStore::init();
+        store.remote_trait("TestTrait");
     }
 
     #[test]
-    fn tick_statuses() {}
-
-    #[test]
-    fn has_status() {}
+    fn tick_statuses() {
+        let mut store = StatusStore::init();
+        store.add_trait("TestTrait");
+        store.add_status("TestStatus", 100);
+        store.apply_ticks(100);
+        assert!(!store.has("TestStatus"));
+        assert!(store.has("TestTrait"));
+    }
 
     #[test]
     fn status_integration_with_ecs() {}
--- src/clash/tick_timer.rs
+++ src/clash/tick_timer.rs
@@ -1,3 +1,5 @@
+use std::cmp;
+
 use serde::{Deserialize, Serialize};
 
 use super::BASE_ACTION_COST;
@@ -6,23 +8,39 @@ use super::BASE_ACTION_COST;
 #[derive(Serialize, Deserialize, Clone)]
 pub struct TickTimer {
     ticks: i32,
+    duration: i32,
 }
 
 impl TickTimer {
     pub fn init() -> TickTimer {
-        TickTimer { ticks: 0 }
+        TickTimer {
+            ticks: 0,
+            duration: BASE_ACTION_COST,
+        }
+    }
+
+    pub fn init_with_duration(duration: i32) -> TickTimer {
+        TickTimer { ticks: 0, duration: duration }
     }
 
     pub fn apply_ticks(&mut self, ticks_to_add: i32) -> bool {
         self.ticks += ticks_to_add;
         // We don't handle double wrap around
-        assert!(self.ticks < (BASE_ACTION_COST * 2));
-        if self.ticks >= BASE_ACTION_COST {
-            self.ticks -= BASE_ACTION_COST;
+        assert!(self.ticks < (self.duration * 2));
+        if self.ticks >= self.duration {
+            self.ticks -= self.duration;
             true
         } else {
             false
         }
+    }
+
+    pub fn duration(&self) -> i32 {
+        self.duration
+    }
+
+    pub fn extend_to_duration(&mut self, duration: i32) {
+        self.duration = cmp::max(self.duration(), duration);
     }
 }
 
@@ -53,5 +71,24 @@ mod tests {
     fn double_wrap_asserts() {
         let mut timer = TickTimer::init();
         timer.apply_ticks(200);
+    }
+
+    #[test]
+    fn custom_duration() {
+        let mut timer = TickTimer::init_with_duration(150);
+        assert_eq!(false, timer.apply_ticks(50));
+        assert_eq!(false, timer.apply_ticks(50));
+        assert_eq!(false, timer.apply_ticks(30));
+        assert_eq!(true, timer.apply_ticks(30));
+    }
+
+    #[test]
+    fn extend_duration() {
+        let mut timer = TickTimer::init();
+        assert_eq!(100, timer.duration());
+        timer.extend_to_duration(50);
+        assert_eq!(100, timer.duration());
+        timer.extend_to_duration(150);
+        assert_eq!(150, timer.duration());
     }
 }
