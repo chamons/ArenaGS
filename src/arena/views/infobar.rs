2cea5f6818c7671cac17b9dcfc4beea7345ace38
Chris Hamons <chris.hamons@xamarin.com>
2020-08-19 22:02:30 -0400

    Refactor infobar for displaying others

--- src/arena/views/infobar.rs
+++ src/arena/views/infobar.rs
@@ -24,8 +24,17 @@ impl InfoBarView {
     fn render_character_info(&self, ecs: &World, canvas: &mut RenderCanvas) -> BoxResult<()> {
         let mut offset = 5;
         let player = find_player(&ecs);
+
+        self.render_character(canvas, ecs, player, &mut offset)?;
+
+        Ok(())
+    }
+
+    fn render_character(&self, canvas: &mut RenderCanvas, ecs: &World, entity: Entity, offset: &mut i32) -> BoxResult<()> {
         let char_infos = &ecs.read_storage::<CharacterInfoComponent>();
-        let char_info = char_infos.grab(player);
+        let char_info = char_infos.grab(entity);
+        let resources = &ecs.read_storage::<SkillResourceComponent>();
+        let resource = resources.grab(entity);
         let defenses = &char_info.character.defenses;
         let health_text = {
             if defenses.absorb != 0 {
@@ -34,25 +43,23 @@ impl InfoBarView {
                 format!("Health: {:.2}/{:.2}", defenses.health, defenses.max_health)
             }
         };
-        self.small_text(canvas, health_text.as_str(), &mut offset)?;
+        self.small_text(canvas, health_text.as_str(), offset)?;
 
         if defenses.max_dodge != 0 {
-            self.small_text(canvas, format!("Dodge : {:.2}/{:.2}", defenses.dodge, defenses.max_dodge).as_str(), &mut offset)?;
+            self.small_text(canvas, format!("Dodge : {:.2}/{:.2}", defenses.dodge, defenses.max_dodge).as_str(), offset)?;
         }
         if defenses.armor != 0 {
-            self.small_text(canvas, format!("Armor: {:.2}", defenses.armor).as_str(), &mut offset)?;
+            self.small_text(canvas, format!("Armor: {:.2}", defenses.armor).as_str(), offset)?;
         }
 
-        let resources = &ecs.read_storage::<SkillResourceComponent>();
-        let resource = resources.grab(player);
-        self.small_text(canvas, format!("Exhaustion: {:.2}", resource.exhaustion).as_str(), &mut offset)?;
+        self.small_text(canvas, format!("Exhaustion: {:.2}", resource.exhaustion).as_str(), offset)?;
 
-        self.small_text(canvas, format!("Focus: {:.2}", resource.focus).as_str(), &mut offset)?;
+        self.small_text(canvas, format!("Focus: {:.2}", resource.focus).as_str(), offset)?;
 
         for kind in AmmoKind::into_enum_iter() {
             match resource.max.get(&kind) {
                 Some(value) => {
-                    self.small_text(canvas, format!("{:?}: {:.2}/{:.2}", kind, resource.ammo[&kind], value).as_str(), &mut offset)?;
+                    self.small_text(canvas, format!("{:?}: {:.2}/{:.2}", kind, resource.ammo[&kind], value).as_str(), offset)?;
                 }
                 None => {}
             }
@@ -60,12 +67,16 @@ impl InfoBarView {
 
         let temperature = char_info.character.temperature.current_temperature();
         if temperature != 0 {
-            self.small_text(canvas, format!("Temperature: {:.2}", temperature).as_str(), &mut offset)?;
+            self.small_text(canvas, format!("Temperature: {:.2}", temperature).as_str(), offset)?;
         }
         Ok(())
     }
 
+    const MAX_INFO_OFFSET: i32 = 380;
     fn small_text(&self, canvas: &mut RenderCanvas, text: &str, offset: &mut i32) -> BoxResult<()> {
+        if *offset > InfoBarView::MAX_INFO_OFFSET {
+            return Ok(());
+        }
         self.text
             .render_text(text, self.position.x + 4, self.position.y + *offset, canvas, FontSize::Small, FontColor::Black)?;
         *offset += 20;
