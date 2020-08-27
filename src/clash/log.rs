2663b3b56a9a5a22bc923946a01d651448980c60
Chris Hamons <chris.hamons@gmail.com>
2020-08-26 20:58:26 -0500

    Fix test

--- src/clash/log.rs
+++ src/clash/log.rs
@@ -104,7 +104,7 @@ mod tests {
     #[test]
     fn add_can_bump_index() {
         let mut log = Log::init();
-        for _ in 0..10 {
+        for _ in 0..LOG_COUNT {
             log.add("Test");
             assert_eq!(log.index, 0);
         }
