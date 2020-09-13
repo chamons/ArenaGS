834969ba3cdb99be3b7a2aa401391ea94980adfb
Chris Hamons <chris.hamons@xamarin.com>
2020-09-13 08:44:14 -0500

    Add sanity comments on after_image sprites

--- src/after_image/sprites.rs
+++ src/after_image/sprites.rs
@@ -1,13 +1,32 @@
-mod background;
-mod bolt;
-mod detailed_character;
-mod large_enemy;
 mod sprite;
-mod standard_character;
-
-pub use background::Background;
-pub use bolt::Bolt;
-pub use detailed_character::{CharacterAnimationState, DetailedCharacter};
-pub use large_enemy::LargeEnemy;
 pub use sprite::{Sprite, SpriteFolderDescription};
+
+// One image
+// Image Size (640 × 640)
+// Blit Size (580, 595)
+mod background;
+pub use background::Background;
+
+// Spritesheet (320 × x) (Always 5 across, variable height)
+// Image Size (64x64)
+// Blit Size (64x64)
+mod bolt;
+pub use bolt::Bolt;
+
+// Many Images (for now https://github.com/chamons/ArenaGS/issues/206) (1 characters, all animation states)
+// Image Size (48x48)
+// Blit Size (96x96)
+mod detailed_character;
+pub use detailed_character::{CharacterAnimationState, DetailedCharacter};
+
+// Spritesheet (3 by 4) (1 character, 4 directions)
+// Image Size (122x122)
+// Blit Size (122x96) (We cut off the shadow bottom for now https://github.com/chamons/ArenaGS/issues/207)
+mod large_enemy;
+pub use large_enemy::LargeEnemy;
+
+// Spritesheet (12 by 8) (8 characters, 4 directions)
+// Image Size (42x36)
+// Blit Size (105x90) [2.5 zoom]
+mod standard_character;
 pub use standard_character::StandardCharacter;
