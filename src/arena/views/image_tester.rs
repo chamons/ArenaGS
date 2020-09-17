3dd67512e2e789d7f7b68bb2b3de94832607e3ca
Chris Hamons <chris.hamons@gmail.com>
2020-09-16 20:30:21 -0500

    Fix

--- src/arena/views/image_tester.rs
+++ src/arena/views/image_tester.rs
@@ -1,47 +1,47 @@
 use std::rc::Rc;
 
-use specs::prelude::*;
-use sdl2::rect::Point as SDLPoint;
-use sdl2::pixels::Color;
 use sdl2::keyboard::Keycode;
 use sdl2::mouse::MouseButton;
+use sdl2::pixels::Color;
+use sdl2::rect::Point as SDLPoint;
+use specs::prelude::*;
 
+use super::{ContextData, View};
 use crate::after_image::{RenderCanvas, RenderContextHolder, TextRenderer};
-use crate::atlas::{BoxResult};
-use crate::conductor::{Scene, StageDirection, EventStatus, Storyteller};
-use super::{View, ContextData};
+use crate::atlas::BoxResult;
+use crate::conductor::{EventStatus, Scene, StageDirection, Storyteller};
 
 pub struct ImageTesterScene {
     view: Box<dyn View>,
-    ecs: World
+    ecs: World,
 }
 
 impl ImageTesterScene {
     pub fn init(_render_context_holder: &RenderContextHolder, _text_renderer: &Rc<TextRenderer>) -> BoxResult<ImageTesterScene> {
-        Ok(ImageTesterScene{ecs: World::new(), view: Box::new(super::view_components::Frame::init (SDLPoint::new (20, 20))?)})
+        Ok(ImageTesterScene {
+            ecs: World::new(),
+            view: Box::new(super::view_components::Frame::init(SDLPoint::new(20, 20))?),
+        })
     }
-
 }
 
 impl Scene for ImageTesterScene {
-    fn handle_key(&mut self, _keycode: Keycode) {
-    }
+    fn handle_key(&mut self, _keycode: Keycode) {}
 
-    fn handle_mouse(&mut self, _x: i32, _y: i32, _button: Option<MouseButton>) {
-    }
+    fn handle_mouse(&mut self, _x: i32, _y: i32, _button: Option<MouseButton>) {}
 
     fn render(&mut self, canvas: &mut RenderCanvas, frame: u64) -> BoxResult<()> {
-        
-        canvas.set_draw_color(Color::from((0, 128, 255)));        
+        canvas.set_draw_color(Color::from((0, 128, 255)));
         canvas.clear();
 
         self.view.render(&self.ecs, canvas, frame, &ContextData::None)?;
-        
+
+        canvas.present();
+
         Ok(())
     }
 
-    fn tick(&mut self, _frame: u64) {
-    }
+    fn tick(&mut self, _frame: u64) {}
 
     fn on_quit(&mut self) -> BoxResult<()> {
         Ok(())
@@ -73,7 +73,5 @@ impl Storyteller for ImageTesterStoryteller {
 
     fn initial_scene(&self) -> Box<dyn Scene> {
         Box::new(ImageTesterScene::init(&self.render_context, &self.text_renderer).unwrap())
-
-
     }
 }
