d9c78e01a183dc146cde36c7129cb0c9db97530d
Chris Hamons <chris.hamons@gmail.com>
2020-09-18 22:17:30 -0500

    Clean up diff

--- src/arena/views/skillbar.rs
+++ src/arena/views/skillbar.rs
@@ -5,7 +5,6 @@ use sdl2::pixels::Color;
 use sdl2::rect::Point as SDLPoint;
 use sdl2::rect::Rect as SDLRect;
 use sdl2::render::Texture;
-
 use specs::prelude::*;
 
 use super::{ContextData, HitTestResult, View};
