7a1f56848ee0b6b7cc1eb2072532df1589066b7d
Chris Hamons <chris.hamons@gmail.com>
2020-10-05 21:48:47 -0500

    No dev hacks for you (#228)
    - Move all development keys to debug builds only
    - Disable console output in release builds as well

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 #![allow(clippy::collapsible_if)]
 #![allow(clippy::single_match)]
 #![allow(clippy::len_zero)]
@@ -5,9 +6,6 @@
 use std::{cell::RefCell, rc::Rc};
 
 use leak::Leak;
-
-// Disable annoying black terminal
-//#![windows_subsystem = "windows"]
 
 #[macro_use]
 extern crate derive_is_enum_variant;
