90f2d2cc7f78be3ae656c2d4300d2bb1002a0332
Chris Hamons <chris.hamons@gmail.com>
2020-10-23 21:57:29 -0500

    Add equipment slot model

--- src/clash/progression.rs
+++ src/clash/progression.rs
@@ -1,5 +1,7 @@
+mod equipment;
 mod progression_component;
 mod skill_tree;
 
+pub use equipment::*;
 pub use progression_component::*;
 pub use skill_tree::*;
---
+++ src/clash/progression/equipment.rs
@@ -0,0 +1,143 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone)]
+pub enum EquipmentKinds {
+    Weapon,
+    Armor,
+    Accessory,
+    Mastery,
+}
+
+#[derive(Deserialize, Serialize, Clone)]
+struct Equipment {
+    pub weapon: Vec<String>,
+    pub weapon_count: u32,
+
+    pub armor: Vec<String>,
+    pub armor_count: u32,
+
+    pub accessory: Vec<String>,
+    pub accessory_count: u32,
+
+    pub mastery: Vec<String>,
+    pub mastery_count: u32,
+}
+
+impl Equipment {
+    pub fn init(weapon_count: u32, armor_count: u32, accessory_count: u32, mastery_count: u32) -> Equipment {
+        Equipment {
+            weapon_count,
+            armor_count,
+            accessory_count,
+            mastery_count,
+            weapon: vec![],
+            armor: vec![],
+            accessory: vec![],
+            mastery: vec![],
+        }
+    }
+
+    fn get(&mut self, kind: EquipmentKinds) -> (u32, &mut Vec<String>) {
+        match kind {
+            EquipmentKinds::Weapon => (self.weapon_count, &mut self.weapon),
+            EquipmentKinds::Armor => (self.armor_count, &mut self.armor),
+            EquipmentKinds::Accessory => (self.accessory_count, &mut self.accessory),
+            EquipmentKinds::Mastery => (self.mastery_count, &mut self.mastery),
+        }
+    }
+
+    pub fn add(&mut self, kind: EquipmentKinds, name: &str) -> bool {
+        let (max, store) = self.get(kind);
+        if store.len() + 1 <= max as usize {
+            store.push(name.to_string());
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn remove(&mut self, kind: EquipmentKinds, index: usize) -> bool {
+        let (_, store) = self.get(kind);
+        if index < store.len() {
+            store.remove(index);
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn swap(&mut self, kind: EquipmentKinds, name: &str, index: usize) -> bool {
+        let (_, store) = self.get(kind);
+        if index < store.len() {
+            store.remove(index);
+            store.insert(index, name.to_string());
+            true
+        } else {
+            false
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn add_empty_space() {
+        let mut equipment = Equipment::init(2, 0, 0, 0);
+        assert!(equipment.add(EquipmentKinds::Weapon, "Test"));
+        assert_eq!("Test", equipment.weapon.get(0).unwrap());
+    }
+
+    #[test]
+    fn add_no_space() {
+        let mut equipment = Equipment::init(2, 0, 0, 0);
+        equipment.weapon = vec!["a".to_string(), "b".to_string()];
+        assert_eq!(false, equipment.add(EquipmentKinds::Weapon, "Test"));
+    }
+
+    #[test]
+    fn add_zero_spaces() {
+        let mut equipment = Equipment::init(0, 0, 0, 0);
+        assert_eq!(false, equipment.add(EquipmentKinds::Weapon, "Test"));
+    }
+
+    #[test]
+    fn remove_has_item() {
+        let mut equipment = Equipment::init(2, 0, 0, 0);
+        equipment.add(EquipmentKinds::Weapon, "Test");
+        equipment.add(EquipmentKinds::Weapon, "Test2");
+        assert!(equipment.remove(EquipmentKinds::Weapon, 1));
+        assert_eq!("Test", equipment.weapon.get(0).unwrap());
+    }
+
+    #[test]
+    fn remove_empty() {
+        let mut equipment = Equipment::init(2, 0, 0, 0);
+        assert_eq!(false, equipment.remove(EquipmentKinds::Weapon, 1));
+    }
+
+    #[test]
+    fn swap_full() {
+        let mut equipment = Equipment::init(3, 0, 0, 0);
+        equipment.add(EquipmentKinds::Weapon, "Test");
+        equipment.add(EquipmentKinds::Weapon, "Test2");
+        equipment.add(EquipmentKinds::Weapon, "Test3");
+        assert!(equipment.swap(EquipmentKinds::Weapon, "Test4", 1));
+        assert_eq!("Test", equipment.weapon.get(0).unwrap());
+        assert_eq!("Test4", equipment.weapon.get(1).unwrap());
+        assert_eq!("Test3", equipment.weapon.get(2).unwrap());
+    }
+
+    #[test]
+    fn swap_empty() {
+        let mut equipment = Equipment::init(2, 0, 0, 0);
+        assert_eq!(false, equipment.swap(EquipmentKinds::Weapon, "Test2", 0));
+    }
+
+    #[test]
+    fn swap_no_space() {
+        let mut equipment = Equipment::init(0, 0, 0, 0);
+        assert_eq!(false, equipment.swap(EquipmentKinds::Weapon, "Test2", 0));
+    }
+}
