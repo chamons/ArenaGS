d4813c15646ceff089835581747b07928f889a69
Chris Hamons <chris.hamons@gmail.com>
2020-10-29 22:08:48 -0500

    Add Weapon Strength/Range

--- src/clash/progression/embattle.rs
+++ src/clash/progression/embattle.rs
@@ -11,30 +11,63 @@ pub fn create_player(ecs: &mut World, skills: &mut SkillsResource, player_positi
     gunslinger::gunslinger_skills(skills);
 }
 
+/*
+UnlocksAbilityMode(String),
+ModifiesSkillRange(i32, String),
+ModifiesSkillStrength(i32, String),
+*/
+
 fn collect_attack_skills<F>(ecs: &World, skills: &mut SkillsResource, get: F)
 where
     F: Fn(&str) -> SkillInfo,
 {
-    let mut attack_count = 0;
+    let mut attacks = vec![];
+    let mut weapon_range = 0;
+    let mut weapon_strength = 0;
 
     for e in ecs.read_resource::<ProgressionComponent>().state.equipment.all() {
         if let Some(e) = e {
             for effect in e.effect {
                 match effect {
                     EquipmentEffect::UnlocksAbilityClass(kind) => {
-                        attack_count += 1;
-                        let mut base = get(&kind);
-                        skills.add(base);
+                        attacks.push(get(&kind));
                     }
+                    EquipmentEffect::ModifiesWeaponRange(delta) => weapon_range += delta,
+                    EquipmentEffect::ModifiesWeaponStrength(delta) => weapon_strength += delta,
                     _ => {}
                 }
             }
         }
     }
 
-    if attack_count == 0 {
-        let mut base = get("Default");
-        skills.add(base);
+    if attacks.len() == 0 {
+        attacks.push(get("Default"));
+    }
+
+    for mut a in attacks {
+        a.range = a.range.map(|r| (r as i32 + weapon_range) as u32);
+
+        let new_damage = |damage: Damage| Damage::init((damage.dice() as i32 + weapon_strength) as u32);
+        a.effect = match a.effect {
+            SkillEffect::RangedAttack(damage, kind) => SkillEffect::RangedAttack(new_damage(damage), kind),
+            SkillEffect::MeleeAttack(damage, kind) => SkillEffect::MeleeAttack(new_damage(damage), kind),
+            SkillEffect::ConeAttack(damage, kind, range) => SkillEffect::ConeAttack(new_damage(damage), kind, range),
+            SkillEffect::ChargeAttack(damage, kind) => SkillEffect::ChargeAttack(new_damage(damage), kind),
+            SkillEffect::MoveAndShoot(damage, range, kind) => SkillEffect::MoveAndShoot(new_damage(damage), range, kind),
+            SkillEffect::Orb(damage, kind, speed, duration) => SkillEffect::Orb(new_damage(damage), kind, speed, duration),
+            SkillEffect::None
+            | SkillEffect::Move
+            | SkillEffect::Reload(_)
+            | SkillEffect::ReloadSome(_, _)
+            | SkillEffect::ReloadSomeRandom(_, _)
+            | SkillEffect::Field(_, _)
+            | SkillEffect::ReloadAndRotateAmmo()
+            | SkillEffect::Buff(_, _)
+            | SkillEffect::Spawn(_)
+            | SkillEffect::SpawnReplace(_)
+            | SkillEffect::Sequence(_, _) => a.effect,
+        };
+        skills.add(a);
     }
 }
 
@@ -82,15 +115,6 @@ fn collect_resource_modifier(ecs: &World) -> Vec<(AmmoKind, i32)> {
     }
     resources.iter().map(|(&kind, &delta)| (kind, delta)).collect()
 }
-
-/*
-UnlocksAbilityClass(String),
-UnlocksAbilityMode(String),
-ModifiesWeaponRange(i32),
-ModifiesSkillRange(i32, String),
-ModifiesWeaponStrength(i32),
-ModifiesSkillStrength(i32, String),
-*/
 
 #[cfg(test)]
 mod tests {
@@ -194,5 +218,49 @@ mod tests {
         });
         assert_eq!(1, skills.skills.len());
         assert_eq!("Triple Shot", skills.get("Triple Shot").name);
+    }
+
+    #[test]
+    fn attack_skills_weapon_range() {
+        let ecs = create_test_state(&[eq(
+            "a",
+            EquipmentKinds::Weapon,
+            &[
+                EquipmentEffect::UnlocksAbilityClass("Triple Shot".to_string()),
+                EquipmentEffect::ModifiesWeaponRange(-1),
+            ],
+            0,
+        )]);
+
+        let mut skills = SkillsResource::init();
+        collect_attack_skills(&ecs, &mut skills, |name| match name {
+            "Triple Shot" => SkillInfo::init_with_distance("Triple Shot", None, TargetType::Any, SkillEffect::None, Some(5), true),
+            _ => panic!(),
+        });
+        assert_eq!(1, skills.skills.len());
+        assert_eq!(Some(4), skills.get("Triple Shot").range);
+    }
+
+    #[test]
+    fn attack_skills_weapon_damage() {
+        let ecs = create_test_state(&[eq("a", EquipmentKinds::Weapon, &[EquipmentEffect::ModifiesWeaponStrength(1)], 0)]);
+
+        let mut skills = SkillsResource::init();
+        collect_attack_skills(&ecs, &mut skills, |name| match name {
+            "Default" => SkillInfo::init_with_distance(
+                "Basic Attack",
+                None,
+                TargetType::Any,
+                SkillEffect::MeleeAttack(Damage::init(3), WeaponKind::Sword),
+                Some(5),
+                true,
+            ),
+            _ => panic!(),
+        });
+        assert_eq!(1, skills.skills.len());
+        match skills.get("Basic Attack").effect {
+            SkillEffect::MeleeAttack(damage, _) => assert_eq!(4, damage.dice()),
+            _ => panic!(),
+        }
     }
 }
