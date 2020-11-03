05f6b2eef9b1aa19d2d9a74f521cbe556b374205
Chris Hamons <chris.hamons@gmail.com>
2020-11-02 21:05:56 -0600

    Wrap up random item selection from pools

--- src/clash/progression/gambler.rs
+++ src/clash/progression/gambler.rs
@@ -1,27 +1,44 @@
-use rand::distributions::{Distribution, Standard};
+use std::cmp;
+
 use rand::prelude::*;
-use rand::Rng;
 use specs::prelude::*;
 
 use super::super::*;
 
-pub fn get_random_items(ecs: &World, player: Entity, requests: Vec<(EquipmentRarity, u32)>) -> Vec<EquipmentItem> {
+pub fn get_random_items(ecs: &World, requests: Vec<(EquipmentRarity, u32)>) -> Vec<EquipmentItem> {
     let equipment = ecs.read_resource::<EquipmentResource>();
     let progression = ecs.read_resource::<ProgressionComponent>();
 
     let available: Vec<&EquipmentItem> = equipment.all().filter(|e| !progression.state.items.contains(&e.name)).collect();
 
-    let common: Vec<&EquipmentItem> = available.iter().filter(|&e| e.rarity == EquipmentRarity::Common).map(|&e| e).collect();
-    let uncommon: Vec<&EquipmentItem> = available.iter().filter(|e| e.rarity == EquipmentRarity::Uncommon).map(|&e| e).collect();
     let rare: Vec<&EquipmentItem> = available.iter().filter(|e| e.rarity == EquipmentRarity::Rare).map(|&e| e).collect();
+    let uncommon: Vec<&EquipmentItem> = available.iter().filter(|e| e.rarity == EquipmentRarity::Uncommon).map(|&e| e).collect();
+    let common: Vec<&EquipmentItem> = available.iter().filter(|&e| e.rarity == EquipmentRarity::Common).map(|&e| e).collect();
 
-    let mut rare_request_count: u32 = requests.iter().filter(|r| r.0 == EquipmentRarity::Rare).map(|r| r.1).sum();
+    let rare_request_count: u32 = requests.iter().filter(|r| r.0 == EquipmentRarity::Rare).map(|r| r.1).sum();
     let mut uncommon_request_count: u32 = requests.iter().filter(|r| r.0 == EquipmentRarity::Uncommon).map(|r| r.1).sum();
     let mut common_request_count: u32 = requests.iter().filter(|r| r.0 == EquipmentRarity::Common).map(|r| r.1).sum();
-    // Sort requests by highest rarity first
-    // Draw from each bucket, if we run out downgrade to a lower rarity and continue
 
-    vec![]
+    let rare_count = cmp::min(rare_request_count, rare.len() as u32);
+    if rare_count < rare_request_count {
+        uncommon_request_count += rare_request_count - rare_count;
+    }
+
+    let uncommon_count = cmp::min(uncommon_request_count, uncommon.len() as u32);
+    if uncommon_count < uncommon_request_count {
+        common_request_count += uncommon_request_count - uncommon_count;
+    }
+
+    let common_count = cmp::min(common_request_count, common.len() as u32);
+
+    let mut rng = ecs.write_resource::<RandomComponent>();
+
+    let mut chosen = Vec::with_capacity((rare_request_count + uncommon_request_count + common_request_count) as usize);
+    chosen.extend(rare.choose_multiple(&mut rng.rand, rare_count as usize).map(|&e| e.clone()));
+    chosen.extend(uncommon.choose_multiple(&mut rng.rand, uncommon_count as usize).map(|&e| e.clone()));
+    chosen.extend(common.choose_multiple(&mut rng.rand, common_count as usize).map(|&e| e.clone()));
+
+    chosen
 }
 
 #[cfg(test)]
@@ -29,11 +46,81 @@ mod tests {
     use super::*;
 
     #[test]
-    fn selects_items() {}
+    fn selects_items() {
+        let mut ecs = World::new();
+
+        let equipments = EquipmentResource::init_with(&[
+            EquipmentItem::init("a", None, EquipmentKinds::Accessory, EquipmentRarity::Common, &[EquipmentEffect::None]),
+            EquipmentItem::init("b", None, EquipmentKinds::Accessory, EquipmentRarity::Common, &[EquipmentEffect::None]),
+            EquipmentItem::init("c", None, EquipmentKinds::Accessory, EquipmentRarity::Common, &[EquipmentEffect::None]),
+            EquipmentItem::init("d", None, EquipmentKinds::Accessory, EquipmentRarity::Common, &[EquipmentEffect::None]),
+        ]);
+
+        let progression = ProgressionComponent::init(ProgressionState::init(0, 0, &["a"], CharacterWeaponKind::Gunslinger, Equipment::init_empty()));
+
+        ecs.insert(RandomComponent::init());
+        ecs.insert(progression);
+        ecs.insert(equipments);
+
+        for _ in 0..10 {
+            let chosen = get_random_items(&ecs, vec![(EquipmentRarity::Common, 2)]);
+            assert_eq!(2, chosen.len());
+            assert!(chosen.iter().all(|c| c.name == "b" || c.name == "c" || c.name == "d"));
+        }
+    }
 
     #[test]
-    fn downgrades_when_too_few() {}
+    fn downgrades_when_too_few() {
+        let mut ecs = World::new();
+
+        let equipments = EquipmentResource::init_with(&[
+            EquipmentItem::init("a", None, EquipmentKinds::Accessory, EquipmentRarity::Common, &[EquipmentEffect::None]),
+            EquipmentItem::init("b", None, EquipmentKinds::Accessory, EquipmentRarity::Common, &[EquipmentEffect::None]),
+            EquipmentItem::init("c", None, EquipmentKinds::Accessory, EquipmentRarity::Common, &[EquipmentEffect::None]),
+            EquipmentItem::init("d", None, EquipmentKinds::Accessory, EquipmentRarity::Uncommon, &[EquipmentEffect::None]),
+            EquipmentItem::init("e", None, EquipmentKinds::Accessory, EquipmentRarity::Uncommon, &[EquipmentEffect::None]),
+            EquipmentItem::init("f", None, EquipmentKinds::Accessory, EquipmentRarity::Rare, &[EquipmentEffect::None]),
+        ]);
+
+        let progression = ProgressionComponent::init(ProgressionState::init(0, 0, &["a"], CharacterWeaponKind::Gunslinger, Equipment::init_empty()));
+
+        ecs.insert(RandomComponent::init());
+        ecs.insert(progression);
+        ecs.insert(equipments);
+
+        for _ in 0..10 {
+            let chosen = get_random_items(
+                &ecs,
+                vec![(EquipmentRarity::Common, 2), (EquipmentRarity::Uncommon, 2), (EquipmentRarity::Rare, 2)],
+            );
+            assert_eq!(5, chosen.len());
+            assert!(chosen.iter().all(|c| c.name != "a"));
+        }
+    }
 
     #[test]
-    fn too_few_total_items() {}
+    fn too_few_total_items() {
+        let mut ecs = World::new();
+
+        let equipments = EquipmentResource::init_with(&[
+            EquipmentItem::init("a", None, EquipmentKinds::Accessory, EquipmentRarity::Common, &[EquipmentEffect::None]),
+            EquipmentItem::init("b", None, EquipmentKinds::Accessory, EquipmentRarity::Common, &[EquipmentEffect::None]),
+            EquipmentItem::init("c", None, EquipmentKinds::Accessory, EquipmentRarity::Common, &[EquipmentEffect::None]),
+        ]);
+
+        let progression = ProgressionComponent::init(ProgressionState::init(0, 0, &["a"], CharacterWeaponKind::Gunslinger, Equipment::init_empty()));
+
+        ecs.insert(RandomComponent::init());
+        ecs.insert(progression);
+        ecs.insert(equipments);
+
+        for _ in 0..10 {
+            let chosen = get_random_items(
+                &ecs,
+                vec![(EquipmentRarity::Common, 12), (EquipmentRarity::Uncommon, 2), (EquipmentRarity::Rare, 2)],
+            );
+            assert_eq!(2, chosen.len());
+            assert!(chosen.iter().all(|c| c.name != "a"));
+        }
+    }
 }
