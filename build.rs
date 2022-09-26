baffc2891781262a3c7b5b7270641bf09b22a44e
Chris Hamons <chris.hamons@gmail.com>
2022-09-25 19:59:10 -0500

    Fix a few windows issues (#281)

--- build.rs
+++ build.rs
@@ -9,25 +9,12 @@ fn print<S: Into<String>>(message: S) {
 fn main() {
     let platform = env::var("CARGO_CFG_TARGET_OS").expect("No Target OS?");
     if let "windows" = platform.as_str() {
-        let lib_dir = Path::new(env!("CARGO_MANIFEST_DIR"))
-            .join("lib")
-            .join("win");
+        let plat_dur = Path::new(env!("CARGO_MANIFEST_DIR")).join("platform").join("win");
 
         let mut res = winres::WindowsResource::new();
-        res.set_manifest_file(
-            lib_dir
-                .join("arena_gunpowder_and_sorcery.manifest")
-                .to_str()
-                .unwrap(),
-        );
-        res.set_icon(
-            lib_dir
-                .join("arena_gunpowder_and_sorcery.ico")
-                .to_str()
-                .unwrap(),
-        );
+        res.set_manifest_file(plat_dur.join("arena_gunpowder_and_sorcery.manifest").to_str().unwrap());
+        res.set_icon(plat_dur.join("arena_gunpowder_and_sorcery.ico").to_str().unwrap());
 
-        res.compile()
-            .expect("Unable to run windows resource compiler");
+        res.compile().expect("Unable to run windows resource compiler");
     }
 }
